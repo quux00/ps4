2d015d341185354b20d95861e6be1efd9776e226
wbthomason <wil.thomason@gmail.com>
2013-12-03 22:17:46 -0500

    Pseudo backspaces and newlines; less idiotic structure for checking for them

--- arch/arm/drivers/mod.rs
+++ arch/arm/drivers/mod.rs
@@ -11,12 +11,14 @@ pub static mut keydown: Option<extern fn(char)> = None;
 #[no_mangle]
 pub unsafe fn keypress() {
     keydown.map(|f| {
-        let x = *io::UART0 as u8 as char;
-	if(x != '\r' && x != 22 as u8 as char) {f(x);}
-	else if (x == 22 as u8 as char) { f(8 as u8 as char); }
-	else {f('\n');}
-    });
-
+        let x = *io::UART0 as u8;
+    	match x {
+    		13			=>	{ f('\n'); }
+    		127			=>	{ f(8 as u8 as char); f(' '); f(8 as u8 as char);}
+    		_			=>	{ f(x as char); }
+    	}
+    }
+	);
     asm!("pop {r11, lr}
           subs pc, lr, #4");
 }
