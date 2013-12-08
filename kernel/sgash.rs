f86bd8f21549d582eb9d162a1be65600f08b7e79
Mister Abc <zl4ry@virginia.edu>
2013-12-07 19:35:36 -0500

    Added c_str to sgash.rs. Strings work\!

--- kernel/sgash.rs
+++ kernel/sgash.rs
@@ -5,11 +5,15 @@ use core::str::*;
 use core::option::{Some, Option, None}; // Match statement
 use core::iter::Iterator;
 use core::vec::Vec;
+use core::mem::Allocator;
+use kernel::*;
 use super::super::platform::*;
 
-pub static mut p_cstr: Option<*mut u8> = None;
-pub static mut p_cstr_i: uint = 0;
-
+pub static mut buffer: cstr = cstr {
+				p: 0 as *mut u8,
+				p_cstr_i: 0,
+				max: 0
+			      };
 pub fn putchar(key: char) {
     unsafe {
 	/*
@@ -27,7 +31,9 @@ fn putstr(msg: &str) {
 	putchar(*c as char);
     }
 }
-pub unsafe fn output()
+
+/*
+pub unsafe fn output(&cstr)
 {
     p_cstr.map(|p| {
 	let mut x = 0;
@@ -38,33 +44,20 @@ pub unsafe fn output()
 	}
     });
 }
+*/
+
 pub unsafe fn parsekey(x: char) {
     let x = x as u8;
     // Set this to false to learn the keycodes of various keys!
     // Key codes are printed backwards because life is hard
     if (true) {
 	match x {
-	    13			=>	{ 
-		prompt(); 
-		//output(); 
-		p_cstr_i = 0; p_cstr.map(|p| {
-		    *(p as *mut char) = '\0';
-		});
+	    13		=>	{ prompt(); }
+	    127		=>	{ 
+		if (buffer.delete_char()) { putchar(''); }
 	    }
-	    127			=>	{ 
-		putchar(''); 
-		p_cstr.map(|p| {
-		    p_cstr_i -= 1;
-		    *(((p as uint)+p_cstr_i) as *mut char) = '\0';
-		});
-	    }
-	    _			=>	{ 
-		putchar(x as char); 
-		p_cstr.map( |p| {
-		    *(((p as uint)+p_cstr_i) as *mut u8) = x;
-		    p_cstr_i += 1;
-		    *(((p as uint)+p_cstr_i) as *mut char) = '\0';
-		}); 
+	    _		=>	{ 
+		if (buffer.add_char(x)) { putchar(x as char); }
 	    }
 	}
     }
@@ -84,8 +77,58 @@ fn keycode(x: u8) {
 
 pub unsafe fn init() {
     prompt();
+    buffer = cstr::new(256);
 }
 
-fn prompt() {
+unsafe fn prompt() {
     putstr(&"\nsgash > ");
+    buffer.reset();
 }
+
+/* BUFFER MODIFICATION FUNCTIONS */
+
+struct cstr {
+    p: *mut u8,
+    p_cstr_i: uint,
+    max: uint 
+}
+
+impl cstr {
+    pub unsafe fn new(size: uint) -> cstr {
+	let (x, y) = memory::allocator.alloc(size);
+	let this = cstr {
+	    p: x,
+	    p_cstr_i: 0,
+	    max: y
+	};
+	this
+    }
+
+    unsafe fn add_char(&mut self, x: u8) -> bool{
+	if (self.p_cstr_i == self.max) { return false; }
+	*(((self.p as uint)+self.p_cstr_i) as *mut u8) = x;
+	self.p_cstr_i += 1;
+	*(((self.p as uint)+self.p_cstr_i) as *mut char) = '\0';
+	true
+    }
+
+    unsafe fn delete_char(&mut self) -> bool {
+	if (self.p_cstr_i == 0) { return false; }
+	self.p_cstr_i -= 1;
+	*(((self.p as uint)+self.p_cstr_i) as *mut char) = '\0';
+	true
+    }
+
+    unsafe fn reset(&mut self) {
+	self.p_cstr_i = 0; 
+	*(self.p as *mut char) = '\0';
+    }
+}
+
+/* Utility functions! */
+/*
+fn eq(a: *mut u8, b: *mut u8) {
+    ind = 0;
+    while (a[i] != 
+}
+*/
