b17cc36231de30551afd24b9f6b8ece930fe5316
John Stevans <john.stevans@outlook.com>
2013-12-10 22:47:45 +0000

    working on hardware cursor. someone halp.

--- main.rs
+++ main.rs
@@ -56,6 +56,7 @@ pub unsafe fn main() {
 */
 	// 640x480
 	// See http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0225d/CACCCFBF.html
+
 	wtm(0x10000010, 0x2C77);
 
 	wtm(0x10120000, 0x3F1F3F9C);
@@ -71,6 +72,19 @@ pub unsafe fn main() {
 	// This doesn't seem to do anything
 	// wtm(0x10008000, 0xF0F0F0F0);
 	
+	//cursor control register
+	wtm(0x10120C00,0xFFFFFFFF);
+	//cursor config register
+	wtm(0x10120C04,0x01010101);
+	//cursor palette register #1
+	wtm(0x10120C08,0x00000000);
+	//cursor palette register #2
+	wtm(0x10120C0C,0xFFFFFFFF);
+	//cursor XY register
+	wtm(0x10120C10, 0x00000000);
+	wtm(0x10120C14, 0x00000000);
+	wtm(0x10120C20, 0xFFFFFFFF);
+	
 	let pl = (1024*1024) as *mut u32;
 	let mut i = 0; 
 	while i < 640*480
@@ -79,6 +93,23 @@ pub unsafe fn main() {
 		*((pl as u32 + i*4) as *mut u32) = 0x00FF0000;
 		i+=1;
 	}
+
+	i = 0;
+	let mut j = 0;
+	while j <= 10000000
+	{
+	    asm!("nop");
+	    j += 1;
+	}
+	    while i < 640*480
+	    {
+		    // not sure about this color scheme
+		    *((pl as u32 + i*4) as *mut u32) = 0x000000FF;
+		    i+=1;
+	    }
+
+	
+
 }
 
 pub unsafe fn wtm(addr: u32, value: u32)
