cad85263a76c5eb1a9c8fe1bfe26285387ca4b15
aml3 <aml3>
2013-12-10 19:58:07 -0500

    rolling our own cursor

--- main.rs
+++ main.rs
@@ -49,39 +49,63 @@ pub unsafe fn main() {
 /*
 	// 800x600
 	// See http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0225d/CACCCFBF.html
-	wtm(0x10000010, 0x2CAC);
-	wtm(0x10120000, 0x1313A4C4);
-	wtm(0x10120004, 0x0505F657);
-	wtm(0x10120008, 0x071F1800);
+	ws(0x10000010, 0x2CAC);
+	ws(0x10120000, 0x1313A4C4);
+	ws(0x10120004, 0x0505F657);
+	ws(0x10120008, 0x071F1800);
 */
 	// 640x480
 	// See http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0225d/CACCCFBF.html
-	wtm(0x10000010, 0x2C77);
+	ws(0x10000010, 0x2C77);
 
-	wtm(0x10120000, 0x3F1F3F9C);
-	wtm(0x10120004, 0x090B61DF);
-	wtm(0x10120008, 0x067F1800);
+	ws(0x10120000, 0x3F1F3F9C);
+	ws(0x10120004, 0x090B61DF);
+	ws(0x10120008, 0x067F1800);
 
 	/* See http://forum.osdev.org/viewtopic.php?p=195000 */
-	wtm(0x10120010, (1*1024*1024));
+	ws(0x10120010, (1*1024*1024));
 
 	/* See http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.ddi0161e/I911024.html */
-	wtm(0x10120018, 0x82B);
-	
-	// This doesn't seem to do anything
-	// wtm(0x10008000, 0xF0F0F0F0);
-	
-	let pl = (1024*1024) as *mut u32;
+	ws(0x10120018, 0x82B);
+/*	
+ *	Turns out this board using the 110 Primecell, not the 111 one. So we can't do stuff with a
+ *	cursor.
+ */
+	/*
 	let mut i = 0; 
+	while i < 0x3FC
+	{
+		wh(0x10120800 + i, 0xFFFFFFFF);
+		i += 1;
+	}
+	ws(0x10120C00, 0x00000002);
+	ws(0x10120C04, 0x00000001);
+	ws(0x10120C20, 0x00000001);
+	*/
+}
+
+pub unsafe fn paint(color: u32)
+{
+	let mut i = 0; 
+	let pl = (1024*1024) as *mut u32;
 	while i < 640*480
 	{
-		// not sure about this color scheme
-		*((pl as u32 + i*4) as *mut u32) = 0x00FF0000;
+		*((pl as u32 + i*4) as *mut u32) = color;
 		i+=1;
 	}
 }
 
-pub unsafe fn wtm(addr: u32, value: u32)
+pub unsafe fn read(addr: u32)	->	u32
+{
+	*(addr as *mut u32)
+}
+
+pub unsafe fn ws(addr: u32, value: u32)
+{
+	*(addr as *mut u32) = *(addr as *mut u32) | value;
+}
+
+pub unsafe fn wh(addr: u32, value: u32)
 {
 	*(addr as *mut u32) = value;
 }
